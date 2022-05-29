e5ec90c0b2f6715a01ad81ca1f320d7edff64fed
Isaac R <isaac.t.rudnick@gmail.com>
2022-05-29 18:50:23 -0400

    Working team score difference

--- analyze_team_score_difference.py
+++ analyze_team_score_difference.py
@@ -3,13 +3,15 @@ import os
 from predictor_highest_score import highest_previous_score
 from predictor_previous_scores import average_previous_scores
 
-path = 'data/teams/'
-# For each event file, get the matches
+# For calculating weighted avg
+# [number, average]
+score_difference_sum = {"count": 0, "total": 0}
 
+path = 'data/teams/'
 dir = os.listdir(path)
 
 for team_file in dir:
-    print(f"Team: {team_file.replace('.json', '')} | {dir.index(team_file)+1}/{len(dir)}")
+    # print(f"Team: {team_file.replace('.json', '')} | {dir.index(team_file)+1}/{len(dir)}")
     f = os.path.join(path, team_file)
     # If not a file, skip this iteration
     if not os.path.isfile(f): continue
@@ -19,5 +21,21 @@ for team_file in dir:
         # If empty file (or other error), skip this iteration
         except json.decoder.JSONDecodeError as e:
             continue
+        
+    # Skip this team if it has no matches
+    if bool([a for a in team_matches.values() if a == []]): continue
+    
+    # Feed fake timestamp of years in future to get all matches
+    team_high_score = highest_previous_score(team_file.replace('.json', ''), 3053863314)
+    # print(f"{team_high_score = }")
+    team_avg_score = average_previous_scores(team_file.replace('.json', ''), 3053863314)
+    # print(f"{team_avg_score = }")
+    
+    try: 
+        difference = (team_high_score / team_avg_score) - 1
+    except: continue
+    
+    score_difference_sum["count"] += 1
+    score_difference_sum["total"] += difference
 
-
+print(f"On average, the highest score for any team was {round(100 * score_difference_sum['total'] / score_difference_sum['count'], 1)}% higher than the average score for that team")
